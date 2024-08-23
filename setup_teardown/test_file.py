0605e5a1ec19f5a00722696e31b1ab3fb7ffecac
Onkar Dhavan <onkardhavan99@gmail.com>
2024-08-23 12:13:12 +0100

    created basic setup and teardown functions

---
+++ setup_teardown/test_file.py
@@ -0,0 +1,23 @@
+def setup_function(function):
+    if function == test1:
+        print("\nSetting up test1.")
+    elif function == test2:
+        print("\nSetting up test2.")
+    else:
+        print("\nSetting up unknown test.")
+
+def teardown_function(function):
+    if function == test1:
+        print("\nTearing down test1.")
+    elif function == test2:
+        print("\nTearing down test2.")
+    else:
+        print("\nTearing down unknown test.")
+
+def test1():
+    print("Execution test1.")
+    assert True
+
+def test2():
+    print("Execution test2.")
+    assert True
